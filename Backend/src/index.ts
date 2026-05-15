342085f6dafbc3e19ea0739e77985b2d828358b4
LuisPalomin05 <pmpalominomedina@gmail.com>
2026-05-15 12:27:14 -0500

    docs: remove outdated and unused project documentation

--- Backend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Fang } from "@fang-js/fang";
-
-const app = new Fang();
-
-app.get("/", (ctx) => {
-  ctx.json({ message: "Fang JS ahora ruge con TypeScript! 🐺" });
-});
-
-app.listen(3000);
